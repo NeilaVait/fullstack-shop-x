09fb8599350b2441c49aefa6dc1f1b6872f5d7e7
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-01 09:27:48 +0300

    empty home, shop, navbar

---
+++ src/pages/shop.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Shop extends Component {
+  render() {
+    return (
+      <div className="shop-page">
+        <h1>I am a shop</h1>
+      </div>
+    );
+  }
+}
+
+export default Shop;
