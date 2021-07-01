86f1cc389cb16d244e94a5ed366eee16d04ca360
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-01 11:38:46 +0300

    added font awesome. Cat list, css vars

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import HeaderX from './components/headerX';
 import Home from './pages/home';
 import Shop from './pages/shop';
+import 'font-awesome/css/font-awesome.css';
 
 class App extends Component {
   state = {
--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 
 class Shop extends Component {
   render() {
+    const { socialLink, shopCategories } = this.props.shop;
     return (
       <div className="shop-page">
         <aside className="categories-aside">
-          <div className="categories"></div>
+          <div className="categories">
+            <ul>
+              {shopCategories.map((item) => (
+                <li key={item._id} className="category-item">
+                  {item.title}
+                </li>
+              ))}
+            </ul>
+          </div>
           <div className="social"></div>
         </aside>
       </div>
