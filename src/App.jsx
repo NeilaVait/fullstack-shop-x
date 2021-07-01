18c110e9ca2364f5d0501948fdc3bcee42597684
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-01 09:41:47 +0300

    links via props

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import HeaderX from './components/headerX';
+import Home from './pages/home';
+import Shop from './pages/shop';
+
+class App extends Component {
+  state = {
+    navLinks: [
+      { to: '/', title: 'Home' },
+      { to: '/shop', title: 'Shop' },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        <HeaderX navLinks={this.state.navLinks} />
+        <Home />
+        <Shop />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/components/headerX.jsx
+++ src/components/headerX.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class HeaderX extends Component {
   render() {
     return (
       <header>
-        <h1>I am a header</h1>
+        <Link to="/">
+          <h3>I am a header</h3>
+        </Link>
+        <nav className="main-nav">{}</nav>
       </header>
     );
   }
