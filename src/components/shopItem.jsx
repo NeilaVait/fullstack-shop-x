dc0792ea32c90ff8064e498cb15bdbae79ae979e
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-01 14:02:12 +0300

    one item css + 3 item in state

--- src/components/shopItem.jsx
+++ src/components/shopItem.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-
+import mainImage from '../static/shop/acc_hat_01_3.jpg';
 class ShopItem extends Component {
   render() {
-    return <div className="shop-item">shop item</div>;
+    return (
+      <div className="shop-item">
+        <img src={mainImage} alt="blue jeans" />
+        <h5 className="item-title">Blue jeans</h5>
+        <p className="item-price">99 eur</p>
+      </div>
+    );
   }
 }
 
