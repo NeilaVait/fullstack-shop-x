0aad875b3557660ba4d8b9eaba22031103f15beb
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-07-08 11:11:20 +0300

    clean state of db data

--- src/App.jsx
+++ src/App.jsx
@@ -16,76 +16,13 @@ class App extends Component {
       { to: '/about', title: 'About' },
     ],
     shop: {
-      shopCategories: [
-        { _id: 1, title: 'Accessories' },
-        { _id: 2, title: 'Denim' },
-        { _id: 3, title: 'Footwear' },
-        { _id: 4, title: 'Jeans' },
-        { _id: 5, title: 'Jackets' },
-      ],
+      shopCategories: [],
       socialLinksData: [
         { to: 'https://www.facebook.com', icon: 'fa fa-facebook', title: 'share' },
         { to: 'https://www.twitter.com', icon: 'fa fa-twitter', title: 'tweet' },
         { to: 'https://www.instagram.com', icon: 'fa fa-instagram', title: 'pin it' },
       ],
-      items: [
-        {
-          _id: 1,
-          title: 'Green hat',
-          price: 99.99,
-          salePrice: 49.99,
-          image: 'acc_hat_01_',
-          color: 'green',
-          sizeQty: [
-            { size: 'small', quantity: 10 },
-            { size: 'medium', quantity: 7 },
-            { size: 'large', quantity: 15 },
-          ],
-          images: [1, 2, 3, 4, 5],
-          sku: 'hat_01',
-          category: 'accessories',
-        },
-        {
-          _id: 2,
-          title: 'Stealth Bomber Jacket',
-          price: 1599.95,
-          image: 'acc_jacket_01_',
-          color: 'navy',
-          size: 'normal',
-          images: [1, 2, 3],
-          category: 'jackets',
-        },
-        {
-          _id: 3,
-          title: 'Feather Slim Fit Denim Jeans',
-          price: 1299.95,
-          image: 'denim_01_',
-          color: 'indigo',
-          size: 'normal',
-          images: [1, 2, 3],
-          category: 'jeans',
-        },
-        {
-          _id: 4,
-          title: 'Suede Combat Boots - Grey',
-          price: 1299.95,
-          image: 'shoe_01_',
-          color: 'grey',
-          size: 'normal',
-          images: [1, 2, 3, 4],
-          category: 'shoes',
-        },
-        {
-          _id: 5,
-          title: 'Suede Combat Boots - Stone',
-          price: 1299.95,
-          image: 'shoe_02_',
-          color: 'stone',
-          size: 'normal',
-          images: [1, 2, 3, 4],
-          category: 'shoes',
-        },
-      ],
+      items: [],
       cart: [
         {
           _id: 1,
@@ -120,8 +57,14 @@ class App extends Component {
     //   .catch((err) => console.log(err));
 
     try {
-      const { data } = await axios.get('http://localhost:4000/api/shop/categories');
-      console.log(data);
+      const categoriesResult = await axios.get('http://localhost:4000/api/shop/categories');
+      const itemsResult = await axios.get('http://localhost:4000/api/shop/items');
+
+      // console.log(data);
+      const shopCopy = { ...this.state.shop };
+      shopCopy.shopCategories = categoriesResult.data;
+      shopCopy.items = itemsResult.data;
+      this.setState({ shop: shopCopy });
     } catch (err) {
       console.log(err);
     }
