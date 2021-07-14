d5593da910cfc5030d93c99102b9df44458c64fd
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-07-14 08:22:54 +0300

    nav burger

--- src/components/headerX.jsx
+++ src/components/headerX.jsx
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Button from './common/button/button';
 
 class HeaderX extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedInUser: '',
+      showNav: false,
+      menuHeight: null,
     };
+    // sukuriam nuoroda i el kaip su getelementbyid
+    this.mainNavRef = React.createRef();
   }
+
+  componentDidMount() {
+    console.log(this.mainNavRef.current.scrollHeight);
+    const currentStateNav = this.state.showNav;
+    this.setState({
+      showNav: !currentStateNav,
+      menuHeight: this.state.showNav ? this.mainNavRef.current.scrollHeight : 0,
+    });
+  }
+
+  toggleBurger = () => {
+    const stateCopy = { ...this.state };
+    stateCopy.showNav = !stateCopy.showNav;
+    this.setState(stateCopy);
+  };
 
   render() {
     return (
@@ -17,14 +34,18 @@ class HeaderX extends Component {
           <Link to="/" className="logo">
             JS3<strong>Shop</strong>X
           </Link>
-          <nav className="main-nav">
+          <nav
+            style={{ maxHeight: this.state.menuHeight }}
+            ref={this.mainNavRef}
+            className={`main-nav ${this.state.showNav ? 'open' : ''}`}
+          >
             {this.props.navLinks.map((item) => (
               <Link key={item.title} className="nav-link-item" to={item.to}>
                 {item.title}
               </Link>
             ))}
           </nav>
-          <button className="mobile-burger">
+          <button onClick={this.toggleBurger} className="mobile-burger">
             <i className="mobile-burger__icon fa fa-bars"></i>
           </button>
           {this.props.currentUser._id && <div className="logged-in">Logged in: {this.props.currentUser.email}</div>}
